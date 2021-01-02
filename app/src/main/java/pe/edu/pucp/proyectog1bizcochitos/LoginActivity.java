dd5df52c6335f7112fd564172b1106f1920caed6
katty <a20160555@pucp.edu.pe>
2021-01-02 00:23:16 -0500

    login registro

--- app/src/main/java/pe/edu/pucp/proyectog1bizcochitos/LoginActivity.java
+++ app/src/main/java/pe/edu/pucp/proyectog1bizcochitos/LoginActivity.java
@@ -2,10 +2,15 @@ package pe.edu.pucp.proyectog1bizcochitos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,11 +22,24 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         Button buttonLoginRegistrate = findViewById(R.id.buttonLoginRegistrate);
+        TextView textViewLoginTitulo = findViewById(R.id.textViewLoginTitulo);
+        TextInputLayout correo = findViewById(R.id.correo);
+        TextInputLayout password = findViewById(R.id.password);
+        Button buttonLoginIniciaSesion = findViewById(R.id.buttonLoginIniciaSesion);
+
         buttonLoginRegistrate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,RegistrateActivity.class);
-                startActivity(intent);
+
+                Pair[] pairs = new Pair[5];
+                pairs[0] = new Pair<View,String>(textViewLoginTitulo,"titulo");
+                pairs[1] = new Pair<View,String>(correo,"correo");
+                pairs[2] = new Pair<View,String>(password,"password");
+                pairs[3] = new Pair<View,String>(buttonLoginIniciaSesion,"button_tran");
+                pairs[4] = new Pair<View,String>(buttonLoginRegistrate,"login_signup");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+                startActivity(intent,options.toBundle());
             }
         });
 
--- app/src/main/java/pe/edu/pucp/proyectog1bizcochitos/RegistrateActivity.java
+++ app/src/main/java/pe/edu/pucp/proyectog1bizcochitos/RegistrateActivity.java
@@ -2,10 +2,15 @@ package pe.edu.pucp.proyectog1bizcochitos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegistrateActivity extends AppCompatActivity {
 
@@ -17,11 +22,24 @@ public class RegistrateActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         Button buttonRegistroLogin = findViewById(R.id.buttonRegistroLogin);
+        TextView textViewRegistroTitulo = findViewById(R.id.textViewRegistroTitulo);
+        TextInputLayout correo = findViewById(R.id.correo);
+        TextInputLayout password = findViewById(R.id.password);
+        Button buttonRegistroSignup = findViewById(R.id.buttonRegistroSignup);
+
         buttonRegistroLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(RegistrateActivity.this,LoginActivity.class);
-                startActivity(intent);
+
+                Pair[] pairs = new Pair[5];
+                pairs[0] = new Pair<View,String>(textViewRegistroTitulo,"titulo");
+                pairs[1] = new Pair<View,String>(correo,"correo");
+                pairs[2] = new Pair<View,String>(password,"password");
+                pairs[3] = new Pair<View,String>(buttonRegistroSignup,"button_tran");
+                pairs[4] = new Pair<View,String>(buttonRegistroLogin,"login_signup");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(RegistrateActivity.this, pairs);
+                startActivity(intent,options.toBundle());
             }
         });
 

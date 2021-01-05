b6326d483c866b587ca6e68e1d42850d92cc90d1
AlonsoGuevara15 <alonso@itic.com.pe>
2021-01-05 16:45:36 -0500

    actividades a usar

---
+++ app/src/main/java/pe/edu/pucp/proyectog1bizcochitos/usuarioTI/DevicesTi.java
@@ -0,0 +1,16 @@
+package pe.edu.pucp.proyectog1bizcochitos.usuarioTI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.edu.pucp.proyectog1bizcochitos.R;
+
+public class DevicesTi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices_ti);
+    }
+}
---
+++ app/src/main/java/pe/edu/pucp/proyectog1bizcochitos/usuarioTI/PedidosTi.java
@@ -0,0 +1,16 @@
+package pe.edu.pucp.proyectog1bizcochitos.usuarioTI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.edu.pucp.proyectog1bizcochitos.R;
+
+public class PedidosTi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedidos_ti);
+    }
+}
